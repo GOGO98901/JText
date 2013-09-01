8b01b244c413b90304b0f55eda649b34710ed6b1
GOGO98901 <GOGO98901@LAP-GOGO98901.home>
2013-09-01 15:42:01 +0100

    Update 2

--- src/gogo98901/net/text/editor/Main.java
+++ src/gogo98901/net/text/editor/Main.java
@@ -2,11 +2,8 @@ package gogo98901.net.text.editor;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/gogo98901/net/text/editor/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author GOGO98901
- *
- */
-package gogo98901.net.text.editor;
